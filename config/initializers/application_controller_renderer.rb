be96e14e6d36f02b1ccceeced1722ab1f6433b82
1noseA <1noseA2L@gmail.com>
2020-02-09 18:45:06 +0900

    [Finish]3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '55debd2d5b7dfa50d686f462ca32e5281dc579dfe841fdec672bbb76bd586a50b5f2b89f1ccd8949be4a9f2b058d1bd24b492922e3aabe3c665aaa7d0179c1d4'
