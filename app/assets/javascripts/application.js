ad7ce7d250f6f3b1172a31501c9fff1750d1b065
1noseA <1noseA2L@gmail.com>
2020-02-26 08:33:19 +0000

    [Finish]6-13

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,16 @@
 //= require_tree .
 
 $(document).ready(function(){
-	$('.jquery').on('click',function(){
-		$(this).css('color','red');
+	$("#theTarget").skippr({
+		transition : 'slide',
+		speed : 1000,
+		easing : 'easeOutQuart',
+		navType : 'block',
+		childrenElementType : 'div',
+		arrows : true,
+		autoPlay :true,
+		autoPlayDuration : 3000,
+		keyboardOnAlways : true,
+		hidePrevious : false
 	});
 });
